420b6af739d0be53b0d9597b481e959d429a081f
jcanterla <jcanterlarufino@safareyes.es>
2023-11-26 11:38:40 +0100

    Mastermind_letras

--- proyecto_mastermind/main.py
+++ proyecto_mastermind/main.py
@@ -54,97 +54,94 @@ def generacion():
     print(palabra2)
     print(numerogenerado)
 
-def masternumeros():
-    def generar_combinacion(longitud):
-    numeros_posibles = ''.join(str(random.randint(0, 9)) for i in range(4))
-    combinacion = []
-    while len(combinacion) < longitud:
-        numero = random.choice(numeros_posibles)
-        if numero not in combinacion:
-            combinacion.append(numero)
-    return combinacion
-
-def jugar():
-    longitud_combinacion = 4
-    combinacion = generar_combinacion(longitud_combinacion)
-    max_intentos = 10
-
-    print("Bienvenido al juego de combinación secreta. Adivina la combinación de {} números.".format(
-        longitud_combinacion))
-    print("Recibirás indicadores 'o', '-', 'x' después de cada dígito introducido.")
-    print("o = número correcto en el lugar correcto")
-    print("- = número correcto en el lugar incorrecto")
-    print("x = número incorrecto")
-    print("")
-
-    intentos = 0
-    while intentos < max_intentos:
-        intento = input("Introduce tu intento ({} dígitos): ".format(longitud_combinacion))
-
-        if len(intento) != longitud_combinacion or not intento.isdigit():
-            print("Por favor, introduce {} números.".format(longitud_combinacion))
-            continue
-
-        verificacion = ""
-        for i in range(longitud_combinacion):
-            if intento[i] == combinacion[i]:
-                verificacion += "o"
-            elif intento[i] in combinacion:
-                verificacion += "-"
-            else:
-                verificacion += "x"
-
-        print("Resultado del intento {}: {}".format(intentos + 1, verificacion))
-
-        if verificacion == 'o' * longitud_combinacion:
-            print("¡Felicidades! Has adivinado la combinación secreta: {}".format(''.join(combinacion)))
-            break
-
-        intentos += 1
-
-    if intentos == max_intentos:
-        print("Lo siento, has alcanzado el número máximo de intentos. La combinación secreta era: {}".format(
-            ''.join(combinacion)))
-    return jugar()
-
-jugar()
-
 
 def masterpalabras():
-    print("\t\t¡Tienes 7 intentos!\n\t\t\t¡Comenzamos!")
+    print("\033[1m" + "APLICACIÓN MASTERMIND" + "\033[0m")
+    nombre = input("Tu nickname, por favor: ")
+    print("¡Comienza el juego para {}".format(nombre))
 
-    palabra = palabra2
-    print(palabra)
-    separar = []
-    for letra in palabra:
-        separar.append(letra)
-    intentos = 7
-    intentos_realizados = 0
+    secuencia = int()
+    if secuencia == int:
+        longitud_combinacion = 4
+        combinacion = numerogenerado
+        max_intentos = 10
+        intentos = 0
+        while intentos < max_intentos:
+            intento = input("Introduce tu intento ({} dígitos): ".format(longitud_combinacion))
 
-    while intentos_realizados < intentos:
-        palabra3 = len(palabra2)
-        guiones = []
+            if len(intento) != longitud_combinacion or not intento.isdigit():
+                print("Por favor, introduce {} números.".format(longitud_combinacion))
+                continue
 
-        for contar in range(palabra3):
-            guiones.append("-")
-        print(guiones)
-        
-        caracter = input("Introduce la palabra que crees que es: ")
-        almacen = []
-        for letra2 in caracter:
-            almacen.append(letra2)
-        caractAlma = len(almacen)
-        for i in range(caractAlma):
-            if almacen[i] == separar[i]:
-                almacen[i] = "o"
-            else:
-                almacen[i] = "-"
-        print(almacen)
-        intentos_realizados += 1
+            verificacion = ""
+            for i in range(longitud_combinacion):
+                if intento[i] == combinacion[i]:
+                    verificacion += "o"
+                elif intento[i] in combinacion:
+                    verificacion += "-"
+                else:
+                    verificacion += "x"
 
-        if palabra == caracter:
-            print("Felicidades")
-            break
+            print("Resultado del intento {}: {}".format(intentos + 1, verificacion))
+
+            if verificacion == 'o' * longitud_combinacion:
+                print("¡Felicidades! Has adivinado la combinación secreta: {}".format(''.join(combinacion)))
+                break
+
+            intentos += 1
+
+        if intentos == max_intentos:
+            print("Lo siento, has alcanzado el número máximo de intentos. La combinación secreta era: {}".format(
+                ''.join(combinacion)))
+
+        print("Bienvenido al juego de combinación secreta. Adivina la combinación de {} números.".format(
+            longitud_combinacion))
+        print("Recibirás indicadores 'o', '-', 'x' después de cada dígito introducido.")
+        print("o = número correcto en el lugar correcto")
+        print("- = número correcto en el lugar incorrecto")
+        print("x = número incorrecto")
+        print("")
+
+    else:
+        print("\033[1m" + "APLICACIÓN MASTERMIND" + "\033[0m")
+        print()
+        print("\t\t¡Tienes 7 intentos!\n\t\t\t¡Comenzamos!")
+
+        palabra = palabra2
+        print(palabra)
+        separar = []
+        for letra in palabra:
+            separar.append(letra)
+
+        intentos = 7
+        intentos_realizados = 0
+
+        while intentos_realizados < intentos:
+
+            caracter = input("Introduce la palabra que crees que es: ")
+            almacen = []
+            for letra2 in caracter:
+                almacen.append(letra2)
+            for i in range(len(almacen)):
+                if almacen[i] == separar[i]:
+                    almacen[i] = "o"
+                elif almacen[i] in separar:
+                    almacen[i] = "-"
+                else:
+                    almacen[i] = "x"
+            print(almacen)
+            intentos_realizados += 1
+
+            if palabra == caracter:
+                print("Has adivinado la combinación")
+                print("¡En {} intentos!".format(intentos_realizados))
+                volver = input("¿Volvemos a jugar (S/N)? ")
+                if volver == "S":
+                    generacion()
+                    masterpalabras()
+
+                else:
+                    break
 
 
 generacion()
